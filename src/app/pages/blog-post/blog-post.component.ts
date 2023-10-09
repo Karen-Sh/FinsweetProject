93cf6c83bb863a70fa565e91bc10046e2479319d
Karen-Sh <1992karen17.04@gmail.com>
2023-10-09 13:01:25 +0400

    blogPost get get posts

--- src/app/pages/blog-post/blog-post.component.ts
+++ src/app/pages/blog-post/blog-post.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JoinComponent } from '../join/join.component';
+import { Post } from 'src/app/models/post';
+import { DataService } from 'src/app/service/data.service';
+import { environment } from 'src/environment/environment';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.css'],
   standalone: true,
-  imports:[JoinComponent]
+  imports:[JoinComponent,NgForOf]
 })
-export class BlogPostComponent {
+export class BlogPostComponent implements OnInit {
+  post: Post[]=[]
+    constructor(private service: DataService){
+
+    }
+  ngOnInit(): void {
+    this.service.GetJsonPost<Post[]>(`${environment.post.get}?_start=16&_end=19`).subscribe(data=>{
+      this.post =data
+    })
+  }
 
 }
